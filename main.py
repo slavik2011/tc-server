0c132e406933787e9cdf120fe79d26a5255b8907
slavik2011 <89517629+slavik2011@users.noreply.github.com>
2024-10-19 13:30:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import sys
 
 app = Flask(__name__)
