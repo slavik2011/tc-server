8bbd933c87184377bca4b4189a479aea28c0fe48
slavik2011 <89517629+slavik2011@users.noreply.github.com>
2024-10-18 21:32:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ def start_typing_task(task_url, cookies_file, req_cps):
     finally:
         if driver:
             socketio.emit('update', {'typed': total_symbols, 'left': 0, 'status': 'Waiting for results...'})
-            time.sleep(7)
+            time.sleep(15)
             # Save the HTML content to a file
             with open(html_file_path, 'w', encoding='utf-8') as f:
                 f.write(driver.page_source)
