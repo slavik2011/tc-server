63db9fd6bc98cfe8b5705e8c5f87dd2ccb61761f
slavik2011 <89517629+slavik2011@users.noreply.github.com>
2024-10-18 19:57:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -176,4 +176,4 @@ def get_status():
     return jsonify({'status': bot_status})
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=int(sys.argv[1]))
+    socketio.run(app, debug=True, port=int(sys.argv[1]), host='0.0.0.0')
