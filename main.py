6f1c5cc658701f5329408cf0d8a76debd8cfd2f0
slavik2011 <89517629+slavik2011@users.noreply.github.com>
2024-10-18 23:04:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 import sys
 
